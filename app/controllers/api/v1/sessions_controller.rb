cef0dca118df3da4a3415257148aceb74c76e6bc
Enzo Fabbiani <enzofab91@hotmail.com>
2021-04-20 19:07:59 -0300

    Add response as user json

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,6 +9,10 @@ module Api
       def resource_params
         params.require(:user).permit(:email, :password)
       end
+
+      def render_create_success
+        render json: { user: resource_data }
+      end
     end
   end
 end
