85e93325afe117c9fda688df3c76d9d34a91776c
Enzo Fabbiani <enzofab91@hotmail.com>
2021-04-23 12:57:02 -0300

    Add support for get all the targets of the current logged user

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -5,6 +5,10 @@ module Api
         @target = current_user.targets.create! targets_params
       end
 
+      def index
+        @targets = current_user.targets.all
+      end
+
       private
 
       def targets_params
