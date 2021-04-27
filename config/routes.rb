5d93526e8f9e10679c462852bcc84e0910a8c67b
Enzo Fabbiani <enzofab91@hotmail.com>
2021-04-27 11:29:25 -0300

    Delete target added

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :targets, only: %i[create index]
+      resources :targets, only: %i[create index destroy]
       resources :topics, only: :index
     end
   end
